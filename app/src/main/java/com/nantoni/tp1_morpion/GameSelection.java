181783204ece6d620d49b922b04b3f0d7c2b0eeb
nantoni <nathan.antoni@outlook.fr>
2019-04-08 14:46:49 +0200

    add button grid

--- app/src/main/java/com/nantoni/tp1_morpion/GameSelection.java
+++ app/src/main/java/com/nantoni/tp1_morpion/GameSelection.java
@@ -1,7 +1,10 @@
 package com.nantoni.tp1_morpion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameSelection extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class GameSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selection);
+
+        Button btn_pvp = (Button) findViewById(R.id.btn_pvp);
+        btn_pvp.setOnClickListener(new View.OnClickListener() {
+            Intent intent = new Intent(GameSelection.this, Game.class);
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
